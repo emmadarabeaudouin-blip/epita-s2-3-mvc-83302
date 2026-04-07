a34188b36aef020c8b53c99f03e5b6164baa7682
Emma Beaudouin <emmadara.beaudouin@gmail.com>
2026-04-07 13:32:19 +0100

    stp

--- VgcCollege.MVC/Controllers/FacultyController.cs
+++ VgcCollege.MVC/Controllers/FacultyController.cs
@@ -1,25 +1,23 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
-using VgcCollege.Domain.Entities;
 using VgcCollege.MVC.Data;
+using VgcCollege.Domain.Entities;
 
 namespace VgcCollege.MVC.Controllers
 {
     public class FacultyController : Controller
     {
-        private readonly AppDbContext _db;
+        private readonly AppDbContext _context;
 
-        public FacultyController(AppDbContext db)
+        public FacultyController(AppDbContext context)
         {
-            _db = db;
+            _context = context;
         }
 
-        // DASHBOARD
-        public IActionResult Index(int id = 1)
+        // Faculty dashboard
+        public IActionResult Index(int id)
         {
-            var faculty = _db.FacultyProfiles
-                .FirstOrDefault(f => f.Id == id);
+            var faculty = _context.FacultyProfiles.FirstOrDefault(f => f.Id == id);
 
             if (faculty == null)
                 return NotFound();
@@ -27,259 +25,157 @@ namespace VgcCollege.MVC.Controllers
             return View(faculty);
         }
 
-        // MY PROFILE
-        public IActionResult MyProfile(int id = 1)
+        // View faculty assigned courses
+        public IActionResult MyCourses(int id)
         {
-            var faculty = _db.FacultyProfiles
-                .FirstOrDefault(f => f.Id == id);
-
-            if (faculty == null)
-                return NotFound();
-
-            return View(faculty);
-        }
-
-        // MY COURSES
-        public IActionResult MyCourses(int id = 1)
-        {
-            var faculty = _db.FacultyProfiles
-                .Include(f => f.CourseAssignments)
-                    .ThenInclude(a => a.Course)
-                        .ThenInclude(c => c.Branch)
-                .FirstOrDefault(f => f.Id == id);
-
-            if (faculty == null)
-                return NotFound();
-
-            return View(faculty);
-        }
-
-        // MY STUDENTS
-        public IActionResult MyStudents(int id = 1)
-        {
-            var faculty = _db.FacultyProfiles
-                .Include(f => f.CourseAssignments)
-                    .ThenInclude(a => a.Course)
-                        .ThenInclude(c => c.Enrolments)
-                            .ThenInclude(e => e.StudentProfile)
-                .FirstOrDefault(f => f.Id == id);
-
-            if (faculty == null)
-                return NotFound();
-
-            return View(faculty);
-        }
-
-        // MY ASSIGNMENTS
-        public IActionResult Assignments(int id = 1)
-        {
-            var assignments = _db.Assignments
-                .Include(a => a.Course)
-                .Where(a => _db.FacultyCourseAssignments.Any(fa =>
-                    fa.FacultyProfileId == id && fa.CourseId == a.CourseId))
+            var courses = _context.FacultyCourseAssignments
+                .Include(fca => fca.Course)
+                .Where(fca => fca.FacultyProfileId == id)
+                .Select(fca => fca.Course)
                 .ToList();
 
             ViewBag.FacultyId = id;
+            return View(courses);
+        }
+
+        // =========================
+        // ADD ASSIGNMENT
+        // =========================
+
+        [HttpGet]
+        public IActionResult AddAssignment(int facultyId)
+        {
+            var assignedCourses = _context.FacultyCourseAssignments
+                .Include(fca => fca.Course)
+                .Where(fca => fca.FacultyProfileId == facultyId)
+                .Select(fca => fca.Course)
+                .ToList();
+
+            ViewBag.FacultyId = facultyId;
+            ViewBag.Courses = assignedCourses;
+
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult AddAssignment(Assignment assignment, int facultyId)
+        {
+            var isAssigned = _context.FacultyCourseAssignments
+                .Any(fca => fca.FacultyProfileId == facultyId && fca.CourseId == assignment.CourseId);
+
+            if (!isAssigned)
+            {
+                ModelState.AddModelError("", "You can only add assignments for your assigned courses.");
+            }
+
+            if (ModelState.IsValid)
+            {
+                _context.Assignments.Add(assignment);
+                _context.SaveChanges();
+                return RedirectToAction("Assignments", new { facultyId = facultyId });
+            }
+
+            var assignedCourses = _context.FacultyCourseAssignments
+                .Include(fca => fca.Course)
+                .Where(fca => fca.FacultyProfileId == facultyId)
+                .Select(fca => fca.Course)
+                .ToList();
+
+            ViewBag.FacultyId = facultyId;
+            ViewBag.Courses = assignedCourses;
+
+            return View(assignment);
+        }
+
+        // View assignments created for assigned courses
+        public IActionResult Assignments(int facultyId)
+        {
+            var assignments = _context.Assignments
+                .Include(a => a.Course)
+                .Where(a => _context.FacultyCourseAssignments
+                    .Any(fca => fca.FacultyProfileId == facultyId && fca.CourseId == a.CourseId))
+                .ToList();
+
+            ViewBag.FacultyId = facultyId;
             return View(assignments);
         }
 
-        // MY EXAMS
-        public IActionResult Exams(int id = 1)
+        // =========================
+        // ADD EXAM
+        // =========================
+
+        [HttpGet]
+        public IActionResult AddExam(int facultyId)
         {
-            var exams = _db.Exams
-                .Include(e => e.Course)
-                .Where(e => _db.FacultyCourseAssignments.Any(fa =>
-                    fa.FacultyProfileId == id && fa.CourseId == e.CourseId))
+            var assignedCourses = _context.FacultyCourseAssignments
+                .Include(fca => fca.Course)
+                .Where(fca => fca.FacultyProfileId == facultyId)
+                .Select(fca => fca.Course)
                 .ToList();
 
-            ViewBag.FacultyId = id;
+            ViewBag.FacultyId = facultyId;
+            ViewBag.Courses = assignedCourses;
+
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult AddExam(Exam exam, int facultyId)
+        {
+            var isAssigned = _context.FacultyCourseAssignments
+                .Any(fca => fca.FacultyProfileId == facultyId && fca.CourseId == exam.CourseId);
+
+            if (!isAssigned)
+            {
+                ModelState.AddModelError("", "You can only add exams for your assigned courses.");
+            }
+
+            if (ModelState.IsValid)
+            {
+                _context.Exams.Add(exam);
+                _context.SaveChanges();
+                return RedirectToAction("Exams", new { facultyId = facultyId });
+            }
+
+            var assignedCourses = _context.FacultyCourseAssignments
+                .Include(fca => fca.Course)
+                .Where(fca => fca.FacultyProfileId == facultyId)
+                .Select(fca => fca.Course)
+                .ToList();
+
+            ViewBag.FacultyId = facultyId;
+            ViewBag.Courses = assignedCourses;
+
+            return View(exam);
+        }
+
+        // View exams for assigned courses
+        public IActionResult Exams(int facultyId)
+        {
+            var exams = _context.Exams
+                .Include(e => e.Course)
+                .Where(e => _context.FacultyCourseAssignments
+                    .Any(fca => fca.FacultyProfileId == facultyId && fca.CourseId == e.CourseId))
+                .ToList();
+
+            ViewBag.FacultyId = facultyId;
             return View(exams);
         }
 
-        // CREATE ASSIGNMENT RESULT (GRADE ASSIGNMENT)
-        public IActionResult CreateAssignmentResult(int assignmentId, int facultyId = 1)
+        public IActionResult MyStudents(int id)
         {
-            var assignment = _db.Assignments
-                .Include(a => a.Course)
-                .FirstOrDefault(a => a.Id == assignmentId);
+            var students = _context.CourseEnrolments
+                  .Include(e => e.StudentProfile)
+                  .Include(e => e.Course)
+                  .Where(e => _context.FacultyCourseAssignments
+                      .Any(fca => fca.FacultyProfileId == id && fca.CourseId == e.CourseId))
+                  .ToList();
 
-            if (assignment == null)
-                return NotFound();
-
-            var isAllowed = _db.FacultyCourseAssignments.Any(fa =>
-                fa.FacultyProfileId == facultyId && fa.CourseId == assignment.CourseId);
-
-            if (!isAllowed)
-                return Forbid();
-
-            var students = _db.CourseEnrolments
-                .Include(e => e.StudentProfile)
-                .Where(e => e.CourseId == assignment.CourseId)
-                .Select(e => e.StudentProfile)
-                .ToList();
-
-            ViewBag.Assignment = assignment;
-            ViewBag.FacultyId = facultyId;
-            ViewBag.Students = new SelectList(students, "Id", "Name");
-
-            var model = new AssignmentResult
-            {
-                AssignmentId = assignmentId
-            };
-
-            return View(model);
+            ViewBag.FacultyId = id;
+            return View(students);
         }
-
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public IActionResult CreateAssignmentResult(AssignmentResult model, int facultyId = 1)
-        {
-            var assignment = _db.Assignments
-                .FirstOrDefault(a => a.Id == model.AssignmentId);
-
-            if (assignment == null)
-                return NotFound();
-
-            var isAllowed = _db.FacultyCourseAssignments.Any(fa =>
-                fa.FacultyProfileId == facultyId && fa.CourseId == assignment.CourseId);
-
-            if (!isAllowed)
-                return Forbid();
-
-            var exists = _db.AssignmentResults.Any(ar =>
-                ar.AssignmentId == model.AssignmentId &&
-                ar.StudentProfileId == model.StudentProfileId);
-
-            if (exists)
-            {
-                ModelState.AddModelError("", "This student already has a result for this assignment.");
-            }
-
-            if (!ModelState.IsValid)
-            {
-                var students = _db.CourseEnrolments
-                    .Include(e => e.StudentProfile)
-                    .Where(e => e.CourseId == assignment.CourseId)
-                    .Select(e => e.StudentProfile)
-                    .ToList();
-
-                ViewBag.Assignment = assignment;
-                ViewBag.FacultyId = facultyId;
-                ViewBag.Students = new SelectList(students, "Id", "Name", model.StudentProfileId);
-
-                return View(model);
-            }
-
-            _db.AssignmentResults.Add(model);
-            _db.SaveChanges();
-
-            TempData["Success"] = "Assignment result posted successfully.";
-            return RedirectToAction(nameof(Assignments), new { id = facultyId });
-        }
-
-        // CREATE EXAM RESULT (GRADE EXAM)
-        public IActionResult CreateExamResult(int examId, int facultyId = 1)
-        {
-            var exam = _db.Exams
-                .Include(e => e.Course)
-                .FirstOrDefault(e => e.Id == examId);
-
-            if (exam == null)
-                return NotFound();
-
-            var isAllowed = _db.FacultyCourseAssignments.Any(fa =>
-                fa.FacultyProfileId == facultyId && fa.CourseId == exam.CourseId);
-
-            if (!isAllowed)
-                return Forbid();
-
-            var students = _db.CourseEnrolments
-                .Include(e => e.StudentProfile)
-                .Where(e => e.CourseId == exam.CourseId)
-                .Select(e => e.StudentProfile)
-                .ToList();
-
-            ViewBag.Exam = exam;
-            ViewBag.FacultyId = facultyId;
-            ViewBag.Students = new SelectList(students, "Id", "Name");
-
-            var model = new ExamResult
-            {
-                ExamId = examId
-            };
-
-            return View(model);
-        }
-
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public IActionResult CreateExamResult(ExamResult model, int facultyId = 1)
-        {
-            var exam = _db.Exams
-                .FirstOrDefault(e => e.Id == model.ExamId);
-
-            if (exam == null)
-                return NotFound();
-
-            var isAllowed = _db.FacultyCourseAssignments.Any(fa =>
-                fa.FacultyProfileId == facultyId && fa.CourseId == exam.CourseId);
-
-            if (!isAllowed)
-                return Forbid();
-
-            var exists = _db.ExamResults.Any(er =>
-                er.ExamId == model.ExamId &&
-                er.StudentProfileId == model.StudentProfileId);
-
-            if (exists)
-            {
-                ModelState.AddModelError("", "This student already has a result for this exam.");
-            }
-
-            if (!ModelState.IsValid)
-            {
-                var students = _db.CourseEnrolments
-                    .Include(e => e.StudentProfile)
-                    .Where(e => e.CourseId == exam.CourseId)
-                    .Select(e => e.StudentProfile)
-                    .ToList();
-
-                ViewBag.Exam = exam;
-                ViewBag.FacultyId = facultyId;
-                ViewBag.Students = new SelectList(students, "Id", "Name", model.StudentProfileId);
-
-                return View(model);
-            }
-
-            _db.ExamResults.Add(model);
-            _db.SaveChanges();
-
-            TempData["Success"] = "Exam result posted successfully.";
-            return RedirectToAction(nameof(Exams), new { id = facultyId });
-        }
-
-        // RELEASE EXAM RESULTS
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public IActionResult ReleaseExamResults(int examId, int facultyId = 1)
-        {
-            var exam = _db.Exams.FirstOrDefault(e => e.Id == examId);
-
-            if (exam == null)
-                return NotFound();
-
-            var isAllowed = _db.FacultyCourseAssignments.Any(fa =>
-                fa.FacultyProfileId == facultyId && fa.CourseId == exam.CourseId);
-
-            if (!isAllowed)
-                return Forbid();
-
-            exam.ResultsReleased = true;
-            _db.Exams.Update(exam);
-            _db.SaveChanges();
-
-            TempData["Success"] = "Exam results released.";
-            return RedirectToAction(nameof(Exams), new { id = facultyId });
-        }
+        
     }
 }
